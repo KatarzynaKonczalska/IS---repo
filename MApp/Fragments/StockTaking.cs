d35e00de21b472113c06767e0522b8de0f04247c
Katarzyna <katarzyna.konczalska@gmail.com>
2017-01-24 13:13:05 +0100

    stock

--- MApp/Fragments/StockTaking.cs
+++ MApp/Fragments/StockTaking.cs
@@ -6,6 +6,7 @@ using Android.Widget;
 using MApp.REST;
 using System.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace MApp.Fragments
 {
@@ -29,6 +30,16 @@ namespace MApp.Fragments
             Button showTag = View.FindViewById<Button>(Resource.Id.button1_StockTaking);
             showTag.Click += OnClick;
             Console.WriteLine(Data.ToString());
+
+            //na liste
+            //List<int> dataList = new List<int>();
+            //for (int i = 0; i < Data.Count-1; i++)
+            //{
+            //    var a = Data[i]["id"];
+            //    string b = a.ToString();
+            //    dataList.Add(int.Parse(b));
+            //}
+            //Console.WriteLine(dataList);
         }
 
         private void OnClick(object sender, EventArgs ea)
