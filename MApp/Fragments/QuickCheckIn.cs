c7f9edbba884b5dd9718cd600ef6efaa2a17d218
Mateusz Nowakowski <mati9821@gmail.com>
2017-02-17 08:18:54 +0100

    a

--- MApp/Fragments/QuickCheckIn.cs
+++ MApp/Fragments/QuickCheckIn.cs
@@ -46,12 +46,6 @@ namespace MApp.Fragments
 
                     dataToSend = "{ \"assetName\":\"" + e1.Text + "\",\"assetAmount\":" + 12 + ",\"assetLocation\": [\"583ea7f9d6194c0c6f51fa70\", 1]" + ",\"assetDetails\":{\"NumerPartii\":\"" + e3.Text + "\",\"Producent\":\"" + e4.Text + "\"}}";
                     string response = await Conn.SendData(dataToSend, Activities.Content.id2);
-                if (!String.IsNullOrEmpty(response))
-                {
-                    TextView temp = View.FindViewById<TextView>(Resource.Id.editText2_QuickCheckIn);
-                    temp.Text = "";
-                    e1.Text = "";
-                }
                 //}
                 //else
                 //{
@@ -59,6 +53,7 @@ namespace MApp.Fragments
                 //}
                 //Po udanym zapisaniu usuwanie numeru id wygenerowanego poprzednio
                 e2.Text = "";
+                e1.Text = "";
             };
 
             Button generate = View.FindViewById<Button>(Resource.Id.button2_QuickCheckIn);
